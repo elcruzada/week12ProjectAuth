ebef2b9f4f7cdedfc02d48d80bb694b374e2a496
Efren Cruzada <efren.laya.cruzada@gmail.com>
2023-02-21 09:27:40 -0600

    models/migrations

---
+++ backend/db/models/review.js
@@ -0,0 +1,37 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Review extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Review.init({
+    spotId: {
+     type: DataTypes.INTEGER,
+     allowNull: false
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    review: {
+     type: DataTypes.STRING,
+     validate: {
+      len: [2,500]
+     }
+    },
+    stars: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Review',
+  });
+  return Review;
+};
