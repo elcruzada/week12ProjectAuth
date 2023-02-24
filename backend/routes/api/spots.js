f6a5d47cfaf118b693deca49b80c303e80286037
Efren Cruzada <efren.laya.cruzada@gmail.com>
2023-02-23 20:29:43 -0600

    up to edit, still need details

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -201,20 +201,256 @@ router.post('/:spotId/images', restoreUser, async (req, res) => {
 router.get('/current', restoreUser, async (req, res) => {
     const currentUserId = req.user.id
 
+
+
     const currentUserSpots = await Spot.findAll({
         where: {
             ownerId: currentUserId
         }
     })
 
+    const spotsWithReviewImage = await Spot.findAll({
+        where: {
+            ownerId: currentUserId
+        },
+        include:[
+            // {
+            //     model: Review
+            // },
+            {
+                model: SpotImage
+            }
+        ],
+
+    })
+
+    const spotsObjects = []
+    for (let i = 0; i < spotsWithReviewImage.length; i++) {
+            const spot = spotsWithReviewImage[i]
+            spotsObjects.push(spot.toJSON())
+    }
+
+    // console.log(spotsObjects)
+
+    for (let j = 0; j < spotsObjects.length; j++) {
+        const spot = spotsObjects[j]
+        // console.log(spot.SpotImages)
+        if (spot.SpotImages.length > 0) {
+            for (let k = 0; k < spot.SpotImages.length; k++) {
+                const image = spot.SpotImages[k]
+                // console.log(image.preview)
+                console.log(spot)
+                if (image.preview === true) {
+                    spot.previewImage = image.url
+                }
+
+                if (!image.preview) {
+                    spot.previewImage = "No preview for this image"
+                }
+            }
+        }
+        delete spot.SpotImages
+
+        //aggregate data
+        let reviewStars = await Review.findOne({
+            where: {
+                spotId: spot.id
+            },
+            attributes: [
+            // {
+                // include: [
+                    [sequelize.fn('AVG', sequelize.col('stars')), 'avgRating']
+                // ]
+            // }
+            ]
+        })
+        // console.log(reviewStars.toJSON())
+        let starAVG = reviewStars.toJSON().avgRating
+
+        if (starAVG) {
+            spot.avgRating = starAVG
+        } else {
+            spot.avgRating = 'No stars yet'
+        }
+    }
 //   const {user} = req.body
 //   if (user) {
 
 //   }
 
-    res.json(currentUserSpots)
+    res.json(spotsObjects)
 })
 
+
+
+router.get('/:spotId', async (req, res) => {
+    const spotIdVar = req.params.spotId
+    const spotDetails = await Spot.findByPk(
+        spotIdVar, {
+        include: {
+            model: SpotImage
+        }
+})
+
+if (!spotDetails) {
+    res.status(404).json({
+        "message": "Spot couldn't be found",
+        "statusCode": 404
+    })
+}
+
+// console.log(spotDetails.SpotImages.length)
+
+// const spotArray = []
+// spotArray.push(spotDetails)
+// console.log(spotArray[0].SpotImages)
+
+// if (spotDetails.SpotImages.length) {
+//     const spotsObjects = []
+//     for (let i = 0; i < spotDetails.length; i++) {
+//         let spot = spotDetails[i]
+//         spotsObjects.push(spot)
+//     }
+//     console.log(spotsObjects)
+// }
+
+            // spotsObjects.push(spotDetails.toJSON())
+
+    // // console.log(spotsObjects)
+
+    // for (let j = 0; j < spotsObjects.length; j++) {
+    //     const spot = spotsObjects[j]
+        // console.log(spot.SpotImages)
+        // if (spotDetails.SpotImages.length > 0) {
+        //     for (let k = 0; k < spotDetails.SpotImages.length; k++) {
+        //         const image = spotDetails.SpotImages[k]
+        //         // console.log(image.preview)
+        //         // console.log(spot)
+        //         if (image.preview === true) {
+        //             spotDetails.previewImage = image.url
+        //         }
+
+        //         if (!image.preview) {
+        //             spotDetails.previewImage = "No preview for this image"
+        //         }
+        //     }
+        // }
+
+        // spotDetails.numReviews = spotDetails.SpotImages.length
+
+        // let reviewStars = await Review.findOne({
+        //     where: {
+        //         spotId: spotDetails.id
+        //     },
+        //     attributes: [
+        //     // {
+        //         // include: [
+        //             [sequelize.fn('AVG', sequelize.col('stars')), 'avgRating']
+        //         // ]
+        //     // }
+        //     ]
+        // })
+        // // console.log(reviewStars.toJSON())
+        // let starAVG = reviewStars.toJSON().avgRating
+
+        // if (starAVG) {
+        //     spotDetails.avgStarRating = starAVG
+        // } else {
+        //     spotDetails.avgStarRating = 'No stars yet'
+        // }
+        // delete spotArray[0].SpotImages
+
+        //aggregate data
+
+        const spotAndId = await Spot.findAll({
+            where: {
+                id: spotIdVar
+            },
+            include: [
+                {
+                    model: Review,
+                    attributes: ['stars']
+                },
+                {
+                model: SpotImage
+                }
+            ]
+        })
+
+
+
+    // console.log(spotDetails)
+
+    // const ownerDetails = await spotDetails.getUser()
+
+    //     console.log(ownerDetails)
+
+    // res.json(spotArray[0])
+    // res.json(ownerDetails)
+    res.json(spotAndId)
+})
+
+router.put('/:spotId', [requireAuth, restoreUser], async (req, res) => {
+    const { address, city, state, country, lat, lng, name, description, price } = req.body
+
+
+
+    let pk = req.params.spotId
+
+    let spotDetails = await Spot.findByPk(pk)
+    if (!spotDetails) {
+        res.status(404).json({
+            "message": "Spot couldn't be found",
+            "statusCode": 404
+        })
+    }
+    console.log(spotDetails)
+    // if (gameName) {
+    //     gameToUpdate.set({
+    //         gameName
+    //     })
+    // }
+    // if (maxPlayers) {
+    //     gameToUpdate.set({
+    //         maxPlayers
+    //     })
+    // }
+    let error = {
+        "message": "Validation Error",
+        "statusCode": 400,
+        "errors": {}
+      }
+
+    if (!address) error.errors.address = "Street address is required"
+    if (!city) error.errors.city = "City is required"
+    if (!state) error.errors.state = "State is required"
+    if (!country) error.errors.country = "Country is required"
+    if (!lat) error.errors.lat = "Latitude is not valid"
+    if (!lng) error.errors.lng = "Longitude is not valid"
+    if (!name) error.errors.name = "Name must be less than 50 characters"
+    if (!description) error.errors.description = "Description is required"
+    if (!price) error.errors.price = "Price per day is required"
+
+    if (!address || !city || !state || !country
+        || !lat || !lng || !name || !description || !price) {
+            res.status(400).json(error)
+        }
+
+    if (address) spotDetails.address = address
+    if (city) spotDetails.city = city
+    if (state) spotDetails.state = state
+    if (country) spotDetails.country = country
+    if (lat) spotDetails.lat = lat
+    if (lng) spotDetails.lng = lng
+    if (name) spotDetails.name = name
+    if (description) spotDetails.description = description
+    if (price) spotDetails.price = price
+
+    await spotDetails.save()
+
+
+    res.json(spotDetails)
+})
 
 
 module.exports = router
