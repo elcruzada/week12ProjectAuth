29051677755e09d4d438d6cd4fa13d142509cfc7
Efren Cruzada <efren.laya.cruzada@gmail.com>
2023-04-09 23:14:05 -0500

    put logic in spotsDetailsCard

--- frontend/src/components/UI/Card/SpotsDetailsCard.js
+++ frontend/src/components/UI/Card/SpotsDetailsCard.js
@@ -1,7 +1,28 @@
 import './SpotsDetailsCard.css'
 
-const SpotsDetailsCard = () => {
+const SpotsDetailsCard = ({spotsDetails, ownerCopy}) => {
 
+    return (
+        <div>
+             <h1>{spotsDetails.name}</h1>
+             <div>
+             {spotsDetails.SpotImages && spotsDetails.SpotImages.map(spotImage => {
+                return (
+                <img src={spotImage.url} alt='location' />
+                )
+            })}
+             </div>
+            <p>
+            {`${spotsDetails.city}, ${spotsDetails.state}, ${spotsDetails.country}`}
+            </p>
+            <p>
+                {`Hosted by: ${ownerCopy.firstName}, ${ownerCopy.lastName}`}
+            </p>
+            <p>
+                {spotsDetails.description}
+            </p>
+        </div>
+    )
 }
 
 export default SpotsDetailsCard
