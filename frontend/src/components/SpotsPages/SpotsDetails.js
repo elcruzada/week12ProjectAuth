3f66430d1300a0c4e503ec55009df7cf4a40abf4
Efren Cruzada <efren.laya.cruzada@gmail.com>
2023-04-11 01:50:54 -0500

    debugged backend code

--- frontend/src/components/SpotsPages/SpotsDetails.js
+++ frontend/src/components/SpotsPages/SpotsDetails.js
@@ -8,26 +8,21 @@ import './SpotsDetails.css'
 const SpotsDetails = () => {
     const dispatch = useDispatch()
     const { spotId } = useParams()
-    const spotsDetails = useSelector(state => state.spots.allSpots)
-    // const spotsDetailsWithId = useSelector(state => console.log(state))
+
+    const singleSpot = useSelector(state => state.spots.singleSpot)
+    // const singleSpot = useSelector(state => console.log(state))
 
     useEffect(() => {
         dispatch(getSpotsDetailsThunk(spotId))
     }, [dispatch, spotId])
 
-    // console.log(Object.values(spotsDetails).length)
-    const owner = spotsDetails.Owner
-    const ownerCopy = {...owner}
-    // const spotsDetailsCopy = {...spotsDetails}
-    // console.log(spotsDetailsCopy)
-    if (!spotsDetails) return null
+    if (!singleSpot) return null
 
-    // console.log(spotsDetails)
-    // console.log(ownerCopy)
-    //prop for spotsDetails
-    //prop for ownerCopy
     return (
-        <SpotsDetailsCard spotsDetails={spotsDetails} ownerCopy={ownerCopy}/>
+        // <></>
+        <SpotsDetailsCard singleSpot={singleSpot} />
+
+
     )
 }
 
