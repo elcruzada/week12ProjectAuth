589d2a666eb1f8675ba92d4e2d073b9decb75941
Efren Cruzada <efren.laya.cruzada@gmail.com>
2023-04-11 13:23:26 -0500

    filtered users in Manage Spots

--- frontend/src/components/SpotsPages/ManageSpots.js
+++ frontend/src/components/SpotsPages/ManageSpots.js
@@ -1,31 +1,33 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { getSpotsDetailsThunk } from "../../store/spots"
+import { getAllSpotsThunk } from "../../store/spots"
 
 const ManageSpots = () => {
     const dispatch = useDispatch()
     const sessionUser = useSelector(state => state.session.user)
-    // console.log(sessionUser.id)
-    // const fetchAllSpotsOfUser = useSelector(state => state.spots.allSpots)
-    // console.log(fetchUserSpots)
-    // console.log(fetchAllSpotsOfUser)
-    // const sessionUserId = {...sessionUser.id}
-    const singleSpot = useSelector(state => state.spots.singleSpot.SpotImages)
-    const singleSpotArray = Object.values(singleSpot)
-    console.log(singleSpotArray)
+    const getAllSpots = useSelector(state => state.spots.allSpots)
+    const iterableAllSpots = Object.values(getAllSpots)
+    // const allSpots = Object.values(getAllSpotsOfUser)[0]
+    // console.log(iterableAllSpots)
+    const userSpots = iterableAllSpots.filter(sessionUserSpot => sessionUser.id === sessionUserSpot.ownerId)
+    console.log(userSpots)
 
     useEffect(() => {
-        dispatch(getSpotsDetailsThunk(sessionUser.id))
+        dispatch(getAllSpotsThunk())
     },[dispatch])
 
     // const singleSpotCopy = {...singleSpot.SpotImages}
     // console.log(singleSpotCopy)
 
+
     return (
         <>
-        {/* {singleSpotCopy.map(spot => {
-            return console.log(spot)
-        })} */}
+        <h1>Manage Your Spots</h1>
+        <h2>Create a New Spot</h2>
+        {userSpots.map(spot => {
+            console.log(spot)
+
+        })}
         </>
     )
 }
