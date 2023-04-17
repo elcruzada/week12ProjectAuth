cfb00386a159758555f5ca41394eff20a27d41a0
Efren Cruzada <efren.laya.cruzada@gmail.com>
2023-04-17 02:04:51 -0500

    fixed spots styling

--- frontend/src/components/UI/Card/SpotsDetailsCard.js
+++ frontend/src/components/UI/Card/SpotsDetailsCard.js
@@ -9,7 +9,7 @@ import { useHistory } from 'react-router-dom'
 
 // import reviewsReducer from '../../../store/reviews'
 
-const SpotsDetailsCard = ({singleSpot, allSpotReviews, sessionUser}) => {
+const SpotsDetailsCard = ({ singleSpot, allSpotReviews, sessionUser }) => {
     const history = useHistory()
     const dispatch = useDispatch()
     const { closeModal, showModal } = useModal()
@@ -40,7 +40,7 @@ const SpotsDetailsCard = ({singleSpot, allSpotReviews, sessionUser}) => {
     // console.log(singleSpot)
     // const {singleSpot} = singleSpot
     //jsonstringify
-    const singleSpotCopy = {...singleSpot}
+    const singleSpotCopy = { ...singleSpot }
     const singleSpotUser = singleSpotCopy.Owner
     // const newUserCopy = {...singleSpotUser}
     // const allSpotReviewsCopy = {...allSpotReviews}
@@ -59,7 +59,7 @@ const SpotsDetailsCard = ({singleSpot, allSpotReviews, sessionUser}) => {
     const sortedReviews = convertedAllSpotReviews.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
     const foundReview = convertedAllSpotReviews.forEach(review => {
         if (review && review.userId && sessionUser && sessionUser.id && review.userId === sessionUser.id) {
-           return review
+            return review
         }
     })
     // console.log(foundReview)
@@ -70,8 +70,8 @@ const SpotsDetailsCard = ({singleSpot, allSpotReviews, sessionUser}) => {
     }
 
     // useEffect(() => {
-        //     console.log(textArea)
-        // },[textArea])
+    //     console.log(textArea)
+    // },[textArea])
 
     const confirmDelete = async (reviewId) => {
         await dispatch(deleteReviewThunk(reviewId)).then(() => {
@@ -85,71 +85,103 @@ const SpotsDetailsCard = ({singleSpot, allSpotReviews, sessionUser}) => {
 
         showModal(
             <div className="modal-container">
-              <div className="modal-content">
-                <h1>Confirm Delete</h1>
-                <p>Are you sure you want to delete this review?</p>
-                <button
-                  className="delete"
-                  onClick={() => confirmDelete(reviewId)}
-                >
-                  Yes (Delete Review)
-                </button>
-                <br />
-                <button className="cancel" onClick={closeModal}>
-                  No (Keep Review)
-                </button>
-              </div>
+                <div className="modal-content">
+                    <h1>Confirm Delete</h1>
+                    <p>Are you sure you want to delete this review?</p>
+                    <button
+                        className="delete"
+                        onClick={() => confirmDelete(reviewId)}
+                    >
+                        Yes (Delete Review)
+                    </button>
+                    <br />
+                    <button className="cancel" onClick={closeModal}>
+                        No (Keep Review)
+                    </button>
+                </div>
             </div>
-          );
+        );
 
     }
 
     return (
 
-        <div className ='spots-details-container'>
-             <h1>{singleSpot.name}</h1>
-             <div>
-             {singleSpot.SpotImages && singleSpot.SpotImages.map(spotImage => {
-                return (
-                <img src={spotImage.url}
-                alt='location'
-                key={spotImage.url}
-                className='spot-details-image'
-                />
-                )
-            })}
-             </div>
-            <p>
-            {`${singleSpot?.city}, ${singleSpot?.state}, ${singleSpot?.country}`}
-            </p>
-            <div>
-            <i className="fa fa-star"></i>
-            <h2>{singleSpot.avgStarRating}</h2>
-            {singleSpot.Reviews && singleSpot.Reviews.length > 0 && <h1>·</h1>}
-            {singleSpot.Reviews && singleSpot.Reviews.length === 1 && <p>1 Review</p>}
-            {singleSpot.Reviews && singleSpot.Reviews.length === 0 && <h2>New</h2>}
-            {singleSpot.Reviews && singleSpot.Reviews.length > 1 && <p>{`${singleSpot.Reviews.length} Reviews`}</p>}
+        <div className='spots-details-container'>
+            <h1>{singleSpot.name}</h1>
+            <div className='image-containr'>
+                <p>
+                    {`${singleSpot?.city}, ${singleSpot?.state}, ${singleSpot?.country}`}
+                </p>
+                {singleSpot.SpotImages && singleSpot.SpotImages.map(spotImage => {
+                    return (
+                        <img src={spotImage.url}
+                            alt='location'
+                            key={spotImage.url}
+                            className='spot-details-image'
+                        />
+                    )
+                })}
             </div>
-            {singleSpotUser?.firstName &&
-             singleSpotUser?.lastName &&
-            <p>
-                {`Hosted by: ${singleSpotUser.firstName}, ${singleSpotUser.lastName}`}
-            </p>
-            }
-            <p>
-                {singleSpot.description}
-            </p>
-            <p>{`$${singleSpot.price}night`}</p>
-            <div>
-            {/* {singleSpot.Reviews && singleSpot.Reviews.length === 0 && <i className="fa-solid fa-period"></i>} */}
-            <i className="fa fa-star"></i>
-            <h2>{singleSpot.avgStarRating}</h2>
+            <div className='div-two-columns-below-image'>
 
-             {singleSpot.Reviews && singleSpot.Reviews.length > 0 && <h1>·</h1>}
-            {singleSpot.Reviews && singleSpot.Reviews.length === 1 && <h2>1 Review</h2>}
-             {singleSpot.Reviews && singleSpot.Reviews.length === 0 && <h2>New</h2>}
-            {singleSpot.Reviews && singleSpot.Reviews.length > 1 && <h2>{`${singleSpot.Reviews.length} Reviews`}</h2>}
+                <div className='column-details-page1'>
+                    {singleSpotUser?.firstName &&
+                        singleSpotUser?.lastName &&
+                        <h2>
+                            {`Hosted by: ${singleSpotUser.firstName}, ${singleSpotUser.lastName}`}
+                        </h2>
+                    }
+                    <div>
+                        {
+                            singleSpot?.description &&
+                            <p>
+                                {singleSpot.description}
+                            </p>
+                        }
+                    </div>
+                </div>
+                <div className='column-details-page2'>
+                    <div className='outerBorder-ratings-container'>
+                        <div className='inner-border-ratings-container'>
+
+                            <div className='left-margin-reserve-box'>
+
+                            <h2>{`$${singleSpot.price} / night`}</h2>
+
+                            </div>
+
+                            <div className='right-margin-reserve-box'>
+                            <i className="fa fa-star"></i>
+
+                            <h3>{singleSpot.avgStarRating}</h3>
+
+                            {singleSpot.Reviews && singleSpot.Reviews.length > 0 && <h1>·</h1>}
+                            {singleSpot.Reviews && singleSpot.Reviews.length === 1 && <h3>1 Review</h3>}
+                            {singleSpot.Reviews && singleSpot.Reviews.length === 0 && <h3>New</h3>}
+                            {singleSpot.Reviews && singleSpot.Reviews.length > 1 && <h3>{`${singleSpot.Reviews.length} Reviews`}</h3>}
+                            </div>
+
+                        </div>
+                        <button onClick={() => alert('Feature coming soon')}
+                        className='reserve-button'
+                        >Reserve</button>
+                    </div>
+                </div>
             </div>
+                <hr style={{ color: 'black', backgroundColor: 'black', height: 1 }} />
+                <div className='details-ratings-container'>
+
+                    <i className="fa fa-star"></i>
+
+                    <h2>{singleSpot.avgStarRating}</h2>
+
+                    {singleSpot.Reviews && singleSpot.Reviews.length > 0 && <h2>·</h2>}
+                    {singleSpot.Reviews && singleSpot.Reviews.length === 1 && <h2>1 Review</h2>}
+                    {singleSpot.Reviews && singleSpot.Reviews.length === 0 && <h2>New</h2>}
+                    {singleSpot.Reviews && singleSpot.Reviews.length > 1 && <h2>{`${singleSpot.Reviews.length} Reviews`}</h2>}
+                </div>
+            {/* {singleSpot.Reviews && singleSpot.Reviews.length === 0 && <i className="fa-solid fa-period"></i>} */}
+
             {
                 sessionUser &&
                 Object.values(sessionUser).length > 0 &&
@@ -157,72 +189,73 @@ const SpotsDetailsCard = ({singleSpot, allSpotReviews, sessionUser}) => {
 
                 !foundReview &&
                 // Object.keys(foundReview).length === 1 &&
-            singleSpot.Reviews &&
-            <div className='post-review-container'>
+                singleSpot.Reviews &&
+                <div className='post-review-container'>
 
-                <OpenModalMenuItem
-                    itemText={
-                    <button className='post-review-container'
-                    >Post your review</button>
-                }
-                    modalComponent={<PostReviewModal
-                        singleSpot={singleSpot}
-                        reviewsUpdatedHandler={reviewsUpdatedHandler}
-                        allSpotReviews={allSpotReviews}
+                    <OpenModalMenuItem
+                        itemText={
+                            <button className='post-review-container'
+                            >Post your review</button>
+                        }
+                        modalComponent={<PostReviewModal
+                            singleSpot={singleSpot}
+                            reviewsUpdatedHandler={reviewsUpdatedHandler}
+                            allSpotReviews={allSpotReviews}
                         />}
-                />
-            </div>
-            // Object.values(allSpotReviewsCopy).length > 0 &&
-            // Object.values(allSpotReviewsCopy).every(review => review.userId !== sessionUser.id) &&
-            // <div className='post-revew-button-container'>
-            //     <button
-            //     className='post-review-button'
-            //     onClick={() => postReviewHandler()}
-            //     >Post Your Review</button>
-            // </div>
+                    />
+                </div>
+                // Object.values(allSpotReviewsCopy).length > 0 &&
+                // Object.values(allSpotReviewsCopy).every(review => review.userId !== sessionUser.id) &&
+                // <div className='post-revew-button-container'>
+                //     <button
+                //     className='post-review-button'
+                //     onClick={() => postReviewHandler()}
+                //     >Post Your Review</button>
+                // </div>
             }
 
             <div className='review-list-container'>
-            {Object.values(allSpotReviews).length === 0 &&
-                sessionUser &&
-                convertedAllSpotReviews &&
-                Object.values(sessionUser).length > 0  &&
-                sessionUser?.id !== singleSpot.ownerId  &&
-                <h3>Be the first to review!</h3>}
-            <ul>
-            {Object.values(allSpotReviews).length > 0 &&
-                sortedReviews.map(review => {
-                    const date = new Date(review.createdAt);
-                    const formattedDate = date.toLocaleString("en-US", {
-                      month: "long",
-                      year: "numeric",
-                    });
-               return (
-                review &&
-                review.User &&
-                review?.id &&
-                sessionUser &&
-                sessionUser?.id &&
-                <div key={review.id}>
-                <li>
-                    {review.User.firstName && <h3>{review.User.firstName}</h3>}
-                    {review.review && <p>{review.review}</p>}
-                    {review.createdAt && <p>{formattedDate}</p>}
-                    {sessionUser.id === review.userId &&
+                {Object.values(allSpotReviews).length === 0 &&
+                    sessionUser &&
+                    convertedAllSpotReviews &&
+                    Object.values(sessionUser).length > 0 &&
+                    sessionUser?.id !== singleSpot.ownerId &&
+                    <h3>Be the first to review!</h3>}
+                <ul>
+                    {Object.values(allSpotReviews).length > 0 &&
+                        sortedReviews.map(review => {
+                            const date = new Date(review.createdAt);
+                            const formattedDate = date.toLocaleString("en-US", {
+                                month: "long",
+                                year: "numeric",
+                            });
+                            return (
+                                review &&
+                                review.User &&
+                                review?.id &&
+                                sessionUser &&
+                                sessionUser?.id &&
+                                <div key={review.id}>
+                                    <li>
+                                        {review.User.firstName && <h3>{review.User.firstName}</h3>}
+                                        {review.review && <p>{review.review}</p>}
+                                        {review.createdAt && <p>{formattedDate}</p>}
+                                        {sessionUser.id === review.userId &&
 
-                    <button
-                        onClick={() => deleteReviewHandler(review.id)}
-                    >Delete</button>
+                                            <button
+                                                className='spots-details-delete-button'
+                                                onClick={() => deleteReviewHandler(review.id)}
+                                            >Delete</button>
 
+                                        }
+                                    </li>
+                                </div>
+                            )
+                        })
                     }
-                </li>
-                </div>
-               )
-            })
-            }
-            </ul>
+                </ul>
             </div>
-        <button onClick={() => alert('Feature coming soon')}>Reserve</button>
+
         </div>
 
 
